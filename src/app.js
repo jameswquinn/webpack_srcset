4b35bdee571a1fad44053bde8f581e156703c98f
jameswquinn <jameswquinn@users.noreply.github.com>
2017-10-02 03:48:54 +0100

    initial upload

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const logo = require('./logo.jpeg?sizes=200w+320w+420w+640w+800w');
+const villianess = require('./villianess.jpeg?sizes=200w+320w+420w+640w+800w');
+
+
+
+
+/*
+var img1 = new Image(); // HTML5 Constructor
+img1.src = 'image1.png';
+img1.alt = 'alt';
+document.body.appendChild(img1);
+
+var img2 = document.createElement('img'); // use DOM HTMLImageElement
+img2.src = 'image2.jpg';
+img2.alt = 'alt text';
+document.body.appendChild(img2);
+
+// using first image in the document
+alert(document.images[0].src);
+*/
+
+//import lazysizes from 'lazysizes';
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var PurifyCSSPlugin = require('purifycss-webpack');
+var path = require("path");
+var glob = require('glob');
+var glob = require('glob-all');
+
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+          {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'srcset-loader',
+            
+        },
+          {
+          loader: 'file-loader',
+          options: {
+            hash: 'sha512',
+            digest: 'hex',
+            outputPath: './images/',
+            name: '[name].[hash].[ext]',
+          },
+        },
+        {
+          loader: 'image-webpack-loader',
+          options: {
+            mozjpeg: {
+              quality: 65,
+            },
+            pngquant: {
+              quality: '65-90',
+              speed: 4,
+            },
+            svgo: {
+              plugins: [{
+                removeViewBox: false,
+              }, {
+                removeEmptyAttrs: false,
+              }],
+            },
+          },
+        }],
+      }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        stats: "errors-only",
+        open: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Project Demo',
+            // minify: {
+            //     collapseWhitespace: true
+            // },
+            hash: true,
+            template: './src/index.html', // Load a custom template (ejs by default see the FAQ for details)
+        }),
+        new ExtractTextPlugin({
+            filename: 'app.css',
+            disable: false,
+            allChunks: true
+        }),
+        new PurifyCSSPlugin({
+      // Give paths to parse for rules. These should be absolute!
+      paths: glob.sync(path.join(__dirname, 'src/*.html')),
+    })
+    ]
+}
