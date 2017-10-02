448858cf33ee69a84260e90362bd3fb440c1eb91
jameswquinn <jameswquinn@users.noreply.github.com>
2017-10-02 12:17:41 +0100

    update

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 const logo = require('./logo.jpeg?sizes=200w+320w+420w+512w+640w+720w+800w+960w+1024w+1166w+1280w+1400w');
 const villianess = require('./villianess.jpeg?sizes=200w+320w+420w+512w+640w+720w+800w+960w+1024w+1166w+1280w+1400w');
+const villianess2 = require('./villianess2.jpeg?sizes=200w+320w+420w+512w+640w+720w+800w+960w+1024w+1166w+1280w+1400w');
+const villianess3 = require('./villianess3.jpeg?sizes=200w+320w+420w+512w+640w+720w+800w+960w+1024w+1166w+1280w+1400w');
+const villianess4 = require('./villianess4.jpeg?sizes=200w+320w+420w+512w+640w+720w+800w+960w+1024w+1166w+1280w+1400w');
 
 
 
-[logo, villianess].forEach((src) => {
+[logo, villianess, villianess2, villianess3, villianess4].forEach((src) => {
   const image = new Image();
   image.srcset = src.srcSet;
   image.src = src.sources['800w'];
   image.sizes = '(max-width: 1400px) 100vw, 1400px';
   image.style = 'width: 100%';
-  document.body.appendChild(image);
+  const container = document.getElementById ("container");
+  container.appendChild (image);
 });
 
 //import lazysizes from 'lazysizes';
